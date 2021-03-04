552959c1d18102bf57e2ed99d5344a52d8be731b
xg2009 <80008641+xg2009@users.noreply.github.com>
2021-03-04 12:04:30 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('app_XG').then((cache) => cache.addAll([
+      '/app_XG/',
+      '/app_XG/index.html',
+      '/app_XG/index.js',
+      '/app_XG/style.css',
+      '/app_XG/images/fox1.jpg',
+      '/app_XG/images/fox2.jpg',
+      '/app_XG/images/fox3.jpg',
+      '/app_XG/images/fox4.jpg',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
